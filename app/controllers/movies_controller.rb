1f0a81a356d71c3e7bfe515971141d85570e7ffc
daiki-okamot <okmtdik0417@icloud.com>
2020-03-17 21:35:53 +0900

    add new and create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,17 @@
 class MoviesController < ApplicationController
+  def new
+    @movie = Movie.new # フォーム用の空のインスタンスを生成する。
+  end
+
+  def create
+    @movie = Movie.new(movie_params) # ストロングパラメータを引数に
+    @movie.save # saveをしてデータベースに保存する。
+    redirect_to @movie # showページにリダイレクト
+  end
+
+  private
+
+  def movie_params # ストロングパラメータを定義する
+    params.require(:movie).permit(:title)
+  end
 end
