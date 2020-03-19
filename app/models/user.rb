fe299556be836a80af81a2f766eaa7873d857e4c
daiki-okamot <okmtdik0417@icloud.com>
2020-03-19 21:37:29 +0900

    add コメントモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :movies, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :name,          presence: true,   length: { maximum: 50 }
   validates :username,      presence: true,   length: { maximum: 50 },   uniqueness: true
   validates :description,   presence: true,   length: { maximum: 1000 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_141301) do
+ActiveRecord::Schema.define(version: 2020_03_19_123130) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "movies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
@@ -36,5 +46,7 @@ ActiveRecord::Schema.define(version: 2020_03_18_141301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "movies"
+  add_foreign_key "comments", "users"
   add_foreign_key "movies", "users"
 end
